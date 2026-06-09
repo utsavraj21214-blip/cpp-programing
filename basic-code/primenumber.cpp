311ebb0a641d38306716c12a72bcbf2f1e389190
utsavraj21214-blip <utsavraj21214@gmail.com>
2026-06-09 23:49:17 +0530

    Add files via upload

---
+++ basic-code/primenumber.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num, i;
+    bool isPrime = true;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num <= 1)
+    {
+        isPrime = false;
+    }
+    else
+    {
+        for (i = 2; i <= num / 2; i++)
+        {
+            if (num % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+
+        }
+    }
+
+    if (isPrime)
+        cout << num << " is a Prime Number.";
+    else
+        cout << num << " is not a Prime Number.";
+
+    return 0;
+}
